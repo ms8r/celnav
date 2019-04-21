b22f9ed6477df2c67e6be288c08f9ef51550631f
markus <mschweit66@gmail.com>
2019-04-21 16:12:05 +0200

    change shebang to /usr/bin/env

--- cnscript.py
+++ cnscript.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 
 """main driver for celnav/cnapp - celestial navigation using PyEphem and aa;
 see doc strings in celnav.py and cnapp.py for details
@@ -6,9 +6,9 @@ see doc strings in celnav.py and cnapp.py for details
 
 import celnav.cnapp as cc
 import Tkinter as tk
-    
+
 app = cc.Application(tk.Tk())
-app.winfo_toplevel().title( "%s %s" % (cc.EXT_APP_NAME, 
+app.winfo_toplevel().title( "%s %s" % (cc.EXT_APP_NAME,
     cc.EXT_APP_VERSION))
 
 app.mainloop()
